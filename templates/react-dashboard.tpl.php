d5d4a2d5c998fa649f950ed886860abee951ae6b
Mariano Carballal <marianocarballal@gmail.com>
2016-04-11 13:27:26 -0300

    Update react-dashboard.tpl.php

--- templates/react-dashboard.tpl.php
+++ templates/react-dashboard.tpl.php
@@ -1,7 +1,7 @@
 
 <?php
-  drupal_add_css( drupal_get_path('module','react_dashboard') . '/app/dist/bundle.css' );
-  drupal_add_js( drupal_get_path('module','react_dashboard') . '/app/dist/bundle.js' );
+  drupal_add_css( drupal_get_path('module','react_dashboard') . '/app/dist/bundle.min.css' );
+  drupal_add_js( drupal_get_path('module','react_dashboard') . '/app/dist/bundle.min.js' );
 ?>
 
 <div id="root"></div>
