03a0e4bdae5a2bf79c371f97a47e6b2620964b73
Paul Walker <pjwalker76@gmail.com>
2016-05-10 16:07:52 -0400

    Update module files from init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,14 @@
 REPO="git@github.com:NuCivic/react-dashboard-boilerplate.git"
+DASH=${PWD##*/}
+
+# rename files
+echo "- Renaming Module files to $DASH";
+mv react_dashboard.info $DASH.info
+mv react_dashboard.module $DASH.module
+
+# rename function calls
+echo "- Renaming function calls to $DASH";
+perl -p -i -e 's/react_dashboard/$DASH/g' ./$DASH.module
 
 # Clone the react dashboard boilerplate app.
 echo "- Cloning boilerplate application."
