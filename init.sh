9931703bc03a837cf64fbd6d346371b0cff206bc
Paul Walker <starsinmypockets@gmail.com>
2016-08-24 10:15:39 -0400

    Update init script to pull 0.3 branch of boilerplate project

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 REPO="git@github.com:NuCivic/react-dashboard-boilerplate.git"
+TAG="remotes/origin/0.3.0-candidate"
 
 # Clone the react dashboard boilerplate app.
 echo "- Cloning boilerplate application."
@@ -7,6 +8,7 @@ git clone ${REPO} ./app
 # Install app.
 echo "- Installing boilerplate application."
 cd ./app
+git checkout $TAG
 npm install
 if [[ $? -ne 0 ]]; then
   echo "ERROR: npm install failed"
